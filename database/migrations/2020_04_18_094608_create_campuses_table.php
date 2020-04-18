e57622c130066d56478469a193eaba8a8d2b2897
Muhammad Haziq <m.haziq1997@gmail.com>
2020-04-18 21:14:31 +0800

    Sebelum cuba middleware

---
+++ database/migrations/2020_04_18_094608_create_campuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname')->nullable();
+            $table->string('shortname')->nullable();
+            $table->string('phonenumber');
+            $table->string('address');
+            $table->integer('photo_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campuses');
+    }
+}
